ef75945a5bea9573ca46c68a8e8498a41674b11c
Ickhyun Kwon <ekcode@icloud.com>
2017-05-03 01:26:47 +0900

    brew install ack

--- install.sh
+++ install.sh
@@ -52,7 +52,8 @@ brew install git \
     tree \
     mongodb \
     yarn \
-    heroku
+    heroku \
+    ack
 
 ## fzf
 echo "Task: configure fzf"
--- zshp.sh
+++
@@ -1,4 +0,0 @@
- if ! grep -q "plugins=\(git\)" ~/.zshrc; then
-     echo "Task: settings for zsh plugins"
-     sed -i '' -e 's/plugins=(git)/plugins=(git autojump)/' ~/.zshrc
- fi
