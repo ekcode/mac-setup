3090db91bdc11aeb4a12ff5c9ba6bc328a17a0a8
Ickhyun Kwon <ekcode@icloud.com>
2017-05-10 14:02:32 +0900

    modify hosts configure

--- tasks/hosts.sh
+++ tasks/hosts.sh
@@ -5,10 +5,15 @@
 ################################################################################
 echo "Task: configure /etc/hosts"
 if ! grep -q "local.publisher.daumtools.com" /etc/hosts; then
-    sudo sh -c 'printf "\n127.0.0.1    local.publisher.daumtools.com" >> /etc/hosts'
-    sudo sh -c 'printf "\n127.0.0.1    local.frontview.publisher.daumtools.com" >> /etc/hosts'
-    sudo sh -c 'printf "\n127.0.0.1    local.publisher.biz.daum.net" >> /etc/hosts'
-    echo " >> /etc/hosts modified"
-else
-    echo " >> already modified"
+    sudo sh -c 'printf "\n127.0.0.1\tlocal.publisher.daumtools.com" >> /etc/hosts'
+    sudo sh -c 'printf "\n127.0.0.1\tlocal.frontview.publisher.daumtools.com" >> /etc/hosts'
+    sudo sh -c 'printf "\n127.0.0.1\tlocal.publisher.biz.daum.net" >> /etc/hosts'
+fi
+
+if ! grep -q "127.0.0.1\t`hostname`" /etc/hosts; then
+    sudo sh -c 'printf "\n\n127.0.0.1\t`hostname`" >> /etc/hosts'
+fi
+
+if ! grep -q "::1\t`hostname`" /etc/hosts; then
+    sudo sh -c 'printf "\n::1\t`hostname`" >> /etc/hosts'
 fi
